5733641eae7631e16ae0603b60a2bf9eded07ef9
bryanrichman <bryanrichman2017@u.northwestern.edu>
2016-01-13 14:51:57 -0600

    Lab2

--- src/lab2.cpp
+++ src/lab2.cpp
@@ -16,9 +16,77 @@ int illegalStatements() {
     return 0;
 }
 
-int main() {
-    illegalStatements();
-    cout << "Start with Exercise 1 (legal/illegal statements)" << endl;
-    cout << "Then work on the exercises and code them up." << endl;
-}
 
+
+
+    int odd_even() {
+        int test;
+        cout << "Enter Integer \n";
+        cin >> test;
+        if (!cin) {
+            simple_error("Input must be integer");
+        }
+        else {
+            if (test % 2 == 0) {
+                cout << "the value " << test << " is even";
+            }
+            else {
+                cout << "the value " << test << " is odd";
+            }
+        }
+        return 0;
+
+    }
+
+    int part2() {
+        string operation;
+        double first;
+        double second;
+        cout << "Enter operation and operands \n";
+        cin >> operation >> first >> second;
+        if (! cin) {
+            cout << "Invalid Inputs";
+        }
+        else {
+            if (operation=="+"){
+                cout << first  << "+" << second << " eqauls " << first + second;
+            }
+            if (operation=="-") {
+                cout << "first " << "-" << " second" << first - second;
+            }
+            if (operation=="*") {
+                cout << "first " << "*" << " second" << first * second;
+            }
+            if (operation=="/") {
+                cout << "first " << "/" << " second" << first / second;
+            }
+
+
+        }
+        return 0;
+    }
+
+    enum gender {m, f};
+    int call_me () {
+        string friend_gender;
+        string friend_name;
+        cout <<"Enter friend name and 0 for male, 1 for female \n";
+        cin >> friend_name >> friend_gender;
+        if (! cin) {
+            simple_error("Invalid Inputs");
+        }
+        else if (friend_gender == m) {
+                cout << "If you see "<< friend_name << " please ask him to call me";
+            }
+        else if (friend_gender == f) {
+            cout << "If you see " << friend_name << " please ask her to call me";
+        }
+
+    }
+
+
+int main() {
+    //odd_even();
+    //part2();
+    call_me();
+}
